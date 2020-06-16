fc51a7f902e3b53995df56eae156bbf35efa87e8
bastienwcs <bastien@wldcodeschool.fr>
2020-06-16 13:56:22 +0200

    fix

--- src/main/java/com/example/security/controller/LoginController.java
+++ src/main/java/com/example/security/controller/LoginController.java
@@ -1,17 +1,20 @@
 package com.example.security.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class LoginController {
 
     @GetMapping("/")
+    @ResponseBody
     public String getUser() {
         return "Welcome User";
     }
 
     @GetMapping("/admin")
+    @ResponseBody
     public String getAdmin() {
         return "Welcome Admin";
     }
