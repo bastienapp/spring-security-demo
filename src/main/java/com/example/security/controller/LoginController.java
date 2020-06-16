ffa652ce500f6c14c4d29baf3edf25e0b41e5931
bastienwcs <bastien@wldcodeschool.fr>
2020-06-16 13:52:08 +0200

    fix

--- src/main/java/com/example/security/controller/LoginController.java
+++ src/main/java/com/example/security/controller/LoginController.java
@@ -1,20 +1,38 @@
 package com.example.security.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class LoginController {
 
-    @RolesAllowed("USER")
+    @GetMapping("/login")
+    public String getLogIn() {
+
+        return "login";
+    }
+
+    @GetMapping(value = "/logout")
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
+    }
+
     @GetMapping("/")
     public String getUser() {
         return "Welcome User";
     }
 
-    @RolesAllowed("ADMIN")
     @GetMapping("/admin")
     public String getAdmin() {
         return "Welcome Admin";
